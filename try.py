5061695dc78556ce2f181d571ea26932f41825df
NikKrasavchik <stachinskiy.nikita@gmail.com>
2025-10-03 04:20:56 +0300

    Change users storage type to dict

--- try.py
+++ try.py
@@ -42,19 +42,30 @@ class User:
         self.queries = []
         self.worker = b""
         self.queryId = b""
+        self.id = b""
+        self.textFull = b""
         self.queryText = b""
+        self.timeFull = b""
+        self.timeQueue = b""
+        self.timeWork = b""
 
     def setQueryId(self, queryId):
         self.queryId = queryId
     
     def setQueryText(self, text):
-        self.queryText = text
+        self.queryText = b"\x20".join(text)
 
-    def setQuery(self, query):
-        self.queries.append(query)
+    def setQuery(self, id, textFull):
+        self.id = id
+        self.textFull = textFull
 
     def setWorker(self, worker):
         self.worker = worker
+
+    def setTime(self, timeFull, timeQueue, timeWork):
+        self.timeFull = timeFull
+        self.timeQueue = timeQueue
+        self.timeWork = timeWork
 
     def getConnection(self):
         return self.connection
@@ -68,65 +79,73 @@ class User:
             f.write(b"Connection: " + self.connection + b"\n")
             f.write(b"IP:         " + self.ip + b"\n")
             f.write(b"Worker:     " + self.worker + b"\n")
-            for query in self.queries:
-                f.write(query.printQuery(filename))
+            f.write(b"-----QUERY-----\n")
+            f.write(b"Id:         " + self.id + b"\n")
+            f.write(b"Text full:  " + self.textFull + b"\n")
+            f.write(b"Text:       " + self.queryText + b"\n")
+            f.write(b"-----TIME------\n")
+            f.write(b"Time full:  " + self.timeFull + b"\n")
+            f.write(b"Time queue: " + self.timeQueue + b"\n")
+            f.write(b"Time work:  " + self.timeWork + b"\n")
             f.write(b"-----user------\n")
             f.write(b"\n\n")
 
-
+def byteTextToHex(byteText):
+    return hex(int(byteText.decode(), 16))
 
 def proccessLog(logLine):
     logData = logLine.split(b' ')[9::]
     global i
 
     try:
-
         if (logData[0] == b'Incoming'):
-            users.append(User(logData))
+            dictusers[hex(int(logData[1][5:-1:].decode(), 16))[2::]] = User(logData)
             
         if (logData[0] == b'On'):
-            for user in users:
-                if user.getConnection() == logData[1][5:-1:]:
-                    user.setQueryId(logData[3][6:-1:])
-                    user.setQueryText(logData[5::])
-                    break
+            dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryId(logData[3][6:-1:])
+            queryText = logData[5::]
+            if queryText[-1][-1] == 10:
+                queryText[-1] = queryText[-1][:-1:]
+            dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryText(queryText)
         
         if (logData[0][:5:] == b'Query'):
-            for user in users:
-                if user.getQueryId() == logData[0][6:-1:]:
-                    queryId = logData[0][6:-1:]
-                    id = logData[1][1:-1:]
-                    text = logData[4][2:(logData[4].find(b'&_')):]
-                    query = Query(queryId, id, text)
-                    queryText = b"\x20".join(user.queryText)
-                    query.setText(queryText[:-1:])
-                    user.setQuery(query)
-                    break
+            queryIdNew = logData[0][6:-1:].decode()
+            queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
+            queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
+            connectionFirst = int(list(dictusers.keys())[0], 16)
+            connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
+            queryId = logData[0][6:-1:]
+            id = logData[1][1:-1:]
+            text = logData[4][2:(logData[4].find(b'&_')):]
+            dictusers[connectionNew].setQuery(id, text)
         
         if (logData[0] == b'Sending'):
-            for user in users:
-                userrr = user
-                if user.getQueryId() == logData[1][6:-1:]:
-                    user.setWorker(logData[4][7:-2:])
-                    break
+            queryIdNew = logData[1][6:-1:].decode()
+            queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
+            queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
+            connectionFirst = int(list(dictusers.keys())[0], 16)
+            connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
+            dictusers[connectionNew].setWorker(logData[4][7:-2:])
         
         if (logData[0] == b'End'):
-            for user in users:
-                for query in user.queries:
-                    if query.id == logData[2][1:-2:]:
-                        timeFull = logData[6]
-                        timeQueue = logData[8]
-                        timeWork = logData[10]
-                        query.setTime(timeFull, timeQueue, timeWork)
-                        break
+            queryIdNew = logData[1][6:-1:].decode()
+            queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
+            queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
+            connectionFirst = int(list(dictusers.keys())[0], 16)
+            connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
+            timeFull = logData[6]
+            timeQueue = logData[8]
+            timeWork = logData[10]
+            dictusers[connectionNew].setTime(timeFull, timeQueue, timeWork)
+                    
 
     except Exception as e:
         pass
         with open('errors.txt', 'ab') as f:
             f.write(b"-----ERROR-----\n")
-            f.write(b"Log line: " + logLine + b"\n")
-            f.write(b"Error:    " + str(e).encode() + b"\n")
-            f.write(b"i:        " + str(i).encode() + b"\n")
+            f.write(b"Log line:     " + logLine + b"\n")
+            f.write(b"Unexpect err: " + str(e).encode() + b"\n")
+            f.write(b"i:            " + str(i).encode() + b"\n")
             f.write(b"-----error-----\n")
             f.write(b"\n\n\n\n")
     i += 1
@@ -134,7 +153,7 @@ def proccessLog(logLine):
         
 
 def readLogs():
-    with open('spcd.log.11', 'rb') as file:
+    with open('spcd.log copy.11', 'rb') as file:
         for line in file:
             proccessLog(line)
 
@@ -142,7 +161,7 @@ def resizeDict():
     firstConn = None
     lastConn = None
 
-    with open('spcd.log.11', 'rb') as file:
+    with open('spcd.log copy.11', 'rb') as file:
         for line in file:
             if line.find(b'Incoming') != -1:
                 logData = line.split(b' ')[9::]
@@ -153,22 +172,24 @@ def resizeDict():
                     lastConn = logData[1][5:-1:]
 
     keys = []
-    for i in range(int(firstConn.decode(), 16), int(lastConn.decode(), 16) + 1):
+    for i in range(int(byteTextToHex(firstConn), 16), int(byteTextToHex(lastConn), 16) + 1):
         keys.append(hex(i)[2::])
 
     dictusers = dict.fromkeys(keys, 0)
 
-    print(dictusers)
+    # print(dictusers)
 
 
 
 def main():
-    os.remove('errors.txt') if os.path.exists('errors.txt') else None
+    # os.remove('errors.txt') if os.path.exists('errors.txt') else None
     os.remove('users.txt') if os.path.exists('users.txt') else None
-    # readLogs()
-    # for user in users:
-        # user.printUser('users.txt')
     resizeDict()
+    readLogs()
+    for user in dictusers:
+        dictusers[user].printUser('users.txt')
+    for user in users:
+        user.printUser('users.txt')
 
 if __name__ == "__main__":
     main()
