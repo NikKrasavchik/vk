22e9f39e5313a7bd9a7687270b194dac99d98259
NikKrasavchik <stachinskiy.nikita@gmail.com>
2025-10-05 21:47:45 +0300

    Finished preprocessing log file

--- try.py
+++ try.py
@@ -1,39 +1,6 @@
 import os
 
-users = []
 dictusers = {}
-i = 0
-
-class Query:
-    def __init__(self, query, id, textFull):
-        self.query = query
-        self.id = id
-        self.textFull = textFull
-        self.text = b""
-        self.timeFull = b""
-        self.timeQueue = b""
-        self.timeWork = b""
-
-    def setTime(self, timeFull, timeQueue, timeWork):
-        self.timeFull = timeFull
-        self.timeQueue = timeQueue
-        self.timeWork = timeWork
-    
-    def setText(self, text):
-        self.text = text
-
-    def printQuery(self, filename):
-        output = b""
-        output += b"-----QUERY-----\n"
-        output += b"Query:      " + self.query + b"\n"
-        output += b"Qid:        " + self.id + b"\n"
-        output += b"Text full:  " + self.textFull + b"\n"
-        output += b"Text:       " + self.text + b"\n"
-        output += b"Time full:  " + self.timeFull + b"\n"
-        output += b"Time queue: " + self.timeQueue + b"\n"
-        output += b"Time work:  " + self.timeWork + b"\n"
-        output += b"-----query-----\n"
-        return output
 
 class User:
     def __init__(self, log):
@@ -94,69 +61,72 @@ class User:
 def byteTextToHex(byteText):
     return hex(int(byteText.decode(), 16))
 
+def detectIncoming(logData):
+    dictusers[hex(int(logData[1][5:-1:].decode(), 16))[2::]] = User(logData)
+
+def detectOn(logData):
+    dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryId(logData[3][6:-1:])
+    queryText = logData[5::]
+    if queryText[-1][-1] == 10:
+        queryText[-1] = queryText[-1][:-1:]
+    dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryText(queryText)
+
+def detectQuery(logData):
+    queryIdNew = logData[0][6:-1:].decode()
+    queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
+    queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
+    connectionFirst = int(list(dictusers.keys())[0], 16)
+    connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
+    queryId = logData[0][6:-1:]
+    id = logData[1][1:-1:]
+    text = logData[4][2:(logData[4].find(b'&_')):]
+    dictusers[connectionNew].setQuery(id, text)
+
+def detectSending(logData):
+    queryIdNew = logData[1][6:-1:].decode()
+    queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
+    queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
+    connectionFirst = int(list(dictusers.keys())[0], 16)
+    connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
+    dictusers[connectionNew].setWorker(logData[4][7:-2:])
+
+def detectEnd(logData):
+    queryIdNew = logData[1][6:-1:].decode()
+    queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
+    queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
+    connectionFirst = int(list(dictusers.keys())[0], 16)
+    connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
+    timeFull = logData[6]
+    timeQueue = logData[8]
+    timeWork = logData[10]
+    dictusers[connectionNew].setTime(timeFull, timeQueue, timeWork)
+
 def proccessLog(logLine):
     logData = logLine.split(b' ')[9::]
-    global i
 
     try:
         if (logData[0] == b'Incoming'):
-            dictusers[hex(int(logData[1][5:-1:].decode(), 16))[2::]] = User(logData)
+            detectIncoming(logData)
             
         if (logData[0] == b'On'):
-            dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryId(logData[3][6:-1:])
-            queryText = logData[5::]
-            if queryText[-1][-1] == 10:
-                queryText[-1] = queryText[-1][:-1:]
-            dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryText(queryText)
-        # return
+            detectOn(logData)
 
         if (logData[0][:5:] == b'Query'):
-            queryIdNew = logData[0][6:-1:].decode()
-            queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
-            queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
-            connectionFirst = int(list(dictusers.keys())[0], 16)
-            connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
-            queryId = logData[0][6:-1:]
-            id = logData[1][1:-1:]
-            text = logData[4][2:(logData[4].find(b'&_')):]
-            dictusers[connectionNew].setQuery(id, text)
-        # return
+            detectQuery(logData)
 
         if (logData[0] == b'Sending'):
-            queryIdNew = logData[1][6:-1:].decode()
-            queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
-            queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
-            connectionFirst = int(list(dictusers.keys())[0], 16)
-            connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
-            dictusers[connectionNew].setWorker(logData[4][7:-2:])
-
-        # return
+            detectSending(logData)
         
         if (logData[0] == b'End'):
-            queryIdNew = logData[1][6:-1:].decode()
-            queryIdFirst = dictusers[list(dictusers.keys())[0]].getQueryId().decode()
-            queryIdDiff = int(queryIdNew, 16) - int(queryIdFirst, 16)
-            connectionFirst = int(list(dictusers.keys())[0], 16)
-            connectionNew = str(hex(connectionFirst + queryIdDiff))[2::]
-            timeFull = logData[6]
-            timeQueue = logData[8]
-            timeWork = logData[10]
-            dictusers[connectionNew].setTime(timeFull, timeQueue, timeWork)
-
-        # return            
+            detectEnd(logData)
 
     except Exception as e:
-        pass
         with open('errors.txt', 'ab') as f:
             f.write(b"-----ERROR-----\n")
             f.write(b"Log line:     " + logLine + b"\n")
             f.write(b"Unexpect err: " + str(e).encode() + b"\n")
-            f.write(b"i:            " + str(i).encode() + b"\n")
             f.write(b"-----error-----\n")
             f.write(b"\n\n\n\n")
-    i += 1
-    
-        
 
 def readLogs():
     with open('spcd.log.11', 'rb') as file:
@@ -183,22 +153,12 @@ def resizeDict():
 
     dictusers = dict.fromkeys(keys, 0)
 
-    # print(dictusers)
-
-# newusers = []
-
 def main():
     os.remove('errors.txt') if os.path.exists('errors.txt') else None
     os.remove('users.txt') if os.path.exists('users.txt') else None
+
     resizeDict()
     readLogs()
-    for user in dictusers:
-        dictusers[user].printUser('users.txt')
-    for user in users:
-        user.printUser('users.txt')
-
-    # for user in dictusers:
-    #     if user.
 
 if __name__ == "__main__":
     main()
