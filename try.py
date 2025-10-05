315c6bebc90b170a4685932203fc15118d2c68da
NikKrasavchik <stachinskiy.nikita@gmail.com>
2025-10-05 23:12:31 +0300

    First finish for task

--- try.py
+++ try.py
@@ -11,16 +11,17 @@ class User:
         self.queryId = b""
         self.id = b""
         self.textFull = b""
-        self.queryText = b""
+        self.textQuery = b""
         self.timeFull = b""
         self.timeQueue = b""
         self.timeWork = b""
+        self.dateTime = b""
 
     def setQueryId(self, queryId):
         self.queryId = queryId
     
     def setQueryText(self, text):
-        self.queryText = b"\x20".join(text)
+        self.textQuery = b"\x20".join(text)
 
     def setQuery(self, id, textFull):
         self.id = id
@@ -34,6 +35,9 @@ class User:
         self.timeQueue = timeQueue
         self.timeWork = timeWork
 
+    def setDateTime(self, dateTime):
+        self.dateTime = dateTime[0][1::] + b'_' + dateTime[1][:-1:]
+
     def getConnection(self):
         return self.connection
 
@@ -46,11 +50,12 @@ class User:
             f.write(b"Connection: " + self.connection + b"\n")
             f.write(b"IP:         " + self.ip + b"\n")
             f.write(b"Worker:     " + self.worker + b"\n")
+            f.write(b"DateTime:   " + self.dateTime + b"\n")
             f.write(b"-----QUERY-----\n")
             f.write(b"Qid:        " + self.queryId + b"\n")
             f.write(b"Id:         " + self.id + b"\n")
             f.write(b"Text full:  " + self.textFull + b"\n")
-            f.write(b"Text:       " + self.queryText + b"\n")
+            f.write(b"Text:       " + self.textQuery + b"\n")
             f.write(b"-----TIME------\n")
             f.write(b"Time full:  " + self.timeFull + b"\n")
             f.write(b"Time queue: " + self.timeQueue + b"\n")
@@ -61,15 +66,16 @@ class User:
 def byteTextToHex(byteText):
     return hex(int(byteText.decode(), 16))
 
-def detectIncoming(logData):
+def detectIncoming(logData, dateTime):
     dictusers[hex(int(logData[1][5:-1:].decode(), 16))[2::]] = User(logData)
+    dictusers[hex(int(logData[1][5:-1:].decode(), 16))[2::]].setDateTime(dateTime)
 
 def detectOn(logData):
     dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryId(logData[3][6:-1:])
-    queryText = logData[5::]
-    if queryText[-1][-1] == 10:
-        queryText[-1] = queryText[-1][:-1:]
-    dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryText(queryText)
+    textQuery = logData[5::]
+    if textQuery[-1][-1] == 10:
+        textQuery[-1] = textQuery[-1][:-1:]
+    dictusers[byteTextToHex(logData[1][5:-1:])[2::]].setQueryText(textQuery)
 
 def detectQuery(logData):
     queryIdNew = logData[0][6:-1:].decode()
@@ -103,10 +109,12 @@ def detectEnd(logData):
 
 def proccessLog(logLine):
     logData = logLine.split(b' ')[9::]
+    logDateTime = logLine.split(b' ')[6:8:]
+    # print(logDateTime)
 
     try:
         if (logData[0] == b'Incoming'):
-            detectIncoming(logData)
+            detectIncoming(logData, logDateTime)
             
         if (logData[0] == b'On'):
             detectOn(logData)
@@ -128,7 +136,7 @@ def proccessLog(logLine):
             f.write(b"-----error-----\n")
             f.write(b"\n\n\n\n")
 
-def readLogs():
+def parseLogs():
     with open('spcd.log.11', 'rb') as file:
         for line in file:
             proccessLog(line)
@@ -153,12 +161,96 @@ def resizeDict():
 
     dictusers = dict.fromkeys(keys, 0)
 
+def determineUsers():
+    unniqueIPs = set()
+
+    for user in dictusers:
+        unniqueIPs.add(dictusers[user].ip)
+    
+    print("Unique IPs:\t\t", len(unniqueIPs))
+    # print(unniqueIPs)
+
+def determineQuery():
+    uniqueQueries = set()
+
+    for user in dictusers:
+        uniqueQueries.add(dictusers[user].textQuery)
+
+    print("Unique Queries:\t\t", len(uniqueQueries))
+
+def determineAverageWords():
+    allWords = 0
+    allQueris = 0
+
+    for user in dictusers:
+        if len(dictusers[user].textFull):
+            allQueris += 1
+            allWords += len(dictusers[user].textQuery.split())
+    
+    averageWords = allWords / allQueris
+
+    print("Average words count:\t", averageWords)
+
+def determineAverageTime():
+    timeCount = 0
+    timeAll = 0
+
+    for user in dictusers:
+        if dictusers[user].timeFull != b"":
+            timeCount += 1
+            timeAll += float(dictusers[user].timeFull.decode())
+    
+    averageTime = timeAll / timeCount
+
+    print("Average time count:\t", averageTime)
+
+def determineMaxTime():
+    maxTime = 0
+
+    for user in dictusers:
+        if dictusers[user].timeFull != b"":
+            currentTime = float(dictusers[user].timeFull.decode())
+            if currentTime > maxTime:
+                maxTime = currentTime
+
+    print("Maximum time:\t\t", maxTime)
+
+def determineAverageQuerySec():
+    dictDateTime = {}
+
+    for user in dictusers:
+        if not (dictusers[user].dateTime in dictDateTime):
+            dictDateTime.update({dictusers[user].dateTime: 1})
+        else:
+            dictDateTime[dictusers[user].dateTime] += 1
+    
+    allDateTimes = 0
+    allQueries = 0
+
+    for dateTime in dictDateTime:
+        allDateTimes += 1
+        allQueries += dictDateTime[dateTime]
+
+    averageQuerySec = allQueries / allDateTimes
+    print("Average queries per sec:", averageQuerySec)
+    
+
+def dataProccessing():
+    determineUsers()
+    determineQuery()
+    determineAverageWords()
+    determineAverageTime()
+    determineMaxTime()
+    determineAverageQuerySec()
+
 def main():
     os.remove('errors.txt') if os.path.exists('errors.txt') else None
     os.remove('users.txt') if os.path.exists('users.txt') else None
 
     resizeDict()
-    readLogs()
+    parseLogs()
+
+    dataProccessing()
 
 if __name__ == "__main__":
     main()
